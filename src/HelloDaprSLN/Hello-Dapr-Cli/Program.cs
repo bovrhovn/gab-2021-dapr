74e0a1c0ab5798b8d11ef9a8f487ef9347fbc3b4
Bojan Vrhovnik <bojan@vrhovnik.net>
2021-09-26 20:33:56 +0200

    added instructions to call the basic demo

--- src/HelloDaprSLN/Hello-Dapr-Cli/Program.cs
+++ src/HelloDaprSLN/Hello-Dapr-Cli/Program.cs
@@ -19,6 +19,12 @@ namespace Hello_Dapr_Cli
 
             AnsiConsole.MarkupLine(
                 $"Your selection is [blue]{whichSideCarToCall}[/], issuing call to that sidecar to get data..");
+            
+            // before calling the below code, call dapr to initiate for the first call
+            // dapr run --app-id hello-dapr --dapr-http-port 3600
+            // or for the second call
+            // dapr run --app-id hello-dapr-custom --dapr-http-port 3700 --components-path C:\Work\Dapr\gab-2021-dapr\src\HelloDaprSLN\Hello-Dapr-Cli\dev-components
+
             var url = whichSideCarToCall == "Default sidecar"
                 ? "http://localhost:3600/v1.0/state/statestore/key1"
                 : "http://localhost:3700/v1.0/state/dev-statestore/key2";
